1f76987620a9a880b581d48d080b0d3a5ff0fcf5
Josh Kraft <josh.h.kraft@gmail.com>
2014-06-01 21:32:10 -0700

    Fix Password Update

--- change_password.php
+++ change_password.php
@@ -7,15 +7,15 @@ if(isset($_POST['new_pass'])){
 	if($_POST['new_pass'] == $_POST['confirm_pass']){
 		$pre_salt = md5($_POST['new_pass']);
 		$salted = md5($pre_salt.$salt);
-		$sql = "UPDATE users SET 
-				password = :password
-			WHERE id = '{$_SESSION['user_id']}'";
-	        $stmt = $bdb->prepare ($sql);
-	        $stmt -> bindParam(':password', $salted);
-	        $stmt -> execute();
-	        $stmt->closeCursor();
-        	unset($stmt);
-        	$message = "Password successfully updated.";
+
+		$params = ['executes' => [
+				'password' => $salted
+			], 
+			'where' => ['id' => $_SESSION['user_id']],
+			'table' => 'users'
+		];
+		$dbhelper->update($params);
+        $message = "Password successfully updated.";
 	} else {
 		$message = "New Password and Confirm password do not match.";
 	}
